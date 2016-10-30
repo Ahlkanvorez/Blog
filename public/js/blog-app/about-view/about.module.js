7ab97ac74612bcb51d25d53be786ec1c24c2cdf8
Robert Mitchell <robert.mitchell36@gmail.com>
2016-10-29 19:20:01 -0500

    Added comments for the About module. This completes all the commenting for the user app.

--- public/js/blog-app/about-view/about.module.js
+++ public/js/blog-app/about-view/about.module.js
@@ -1,6 +1,8 @@
 (function () {
 	'use strict';
 
-	// Register the aboutView module.
+	/** Defines the 'aboutView' module, which encapsulates the View and Controller for the static About page.
+	 * The About view is a static HTML page, and consequently this module has no dependencies and an empty Controller.
+	 */
 	angular.module('aboutView', []);
 })();
