312816bd3da64514a62ad9f73dd2ad8b2a34a540
Robert Mitchell <robert.mitchell36@gmail.com>
2016-10-31 17:54:30 -0500

    Commented the article.model.js and category.model.js files for the backend.

--- models/category.model.js
+++ models/category.model.js
@@ -4,12 +4,20 @@
 
     var Schema = mongoose.Schema;
 
-    /* Define the contents of a Category, in which articles are grouped. */
+    /**
+     * Defines the contents of a Category, in which articles are grouped.
+     * The 'name' attribute is displayed in the browser as the title of the category, and the 'description' is displayed
+     * below the name on the main page for viewing a category.
+     * The 'aboutAuthor' attribute is used in the paragraph below the name of the category in the right-column of both
+     * the article-list and article-view pages, and describes me in terms relevant to the given category.
+     * The 'private' attribute is used to mark categories which do not contain any public articles, viz. are still under
+     * construction, and are only visible on the admin page behind the login.
+     */
     var categorySchema = new Schema({
         name: {
             type: String,
             required: true,
-            index: {unique: true}
+            index: { unique: true }
         },
         description: {
             type: String,
@@ -25,17 +33,37 @@
         }
     });
 
+    /**
+     * Filters out all private categories, and returns the public ones.
+     *
+     * @returns all the categories in the database which are public (not private).
+     */
     categorySchema.query.public = function onlyPublic() {
         return this.find({private: false});
     }
 
+    /**
+     * Searches for categories whose names match the given pattern (regular expression).
+     *
+     * @param name The name or regex to match against the name, of the desired categories.
+     * @returns the categories whose name match the given pattern.
+     */
     categorySchema.query.byName = function queryByName(name) {
         return this.find({name: new RegExp(name, 'i')});
     };
 
+    /**
+     * Searches for categories whose description contains the provided pattern (regular expression).
+     *
+     * @param description The description or regex to match against the description, of the desired categories.
+     * @returns the categories whose descriptions match the given pattern.
+     */
     categorySchema.query.byDescription = function queryByDescription(description) {
         return this.find({description: new RegExp(description, 'i')});
     };
 
+    /**
+     * Export a Category model object, based on the above schema, with the above query functions.
+     */
     module.exports.Category = mongoose.model('Category', categorySchema);
 })();
