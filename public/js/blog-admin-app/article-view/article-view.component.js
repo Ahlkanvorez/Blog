71a16fcf873a534ff408b75d1ae7bade7bf8d237
Robert Mitchell <robert.mitchell36@gmail.com>
2016-11-01 18:25:29 -0500

    Finished commenting the article-view module and component, and removed a few left-over functions for use with comments, which I have decided not to implement.

--- public/js/blog-admin-app/article-view/article-view.component.js
+++ public/js/blog-admin-app/article-view/article-view.component.js
@@ -1,208 +1,178 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.
-    module('articleView').
-    component('articleView', {
-      templateUrl : '/js/blog-admin-app/article-view/article-view.template.html',
-      controller : ['$sanitize', '$window', '$scope', '$routeParams', 'ArticleIndex', 'ServerConfig', 'ViewModes', 'Category',
-        function articleViewController($sanitize, $window, $scope, $routeParams, ArticleIndex, ServerConfig, ViewModes, Category) {
-          var self = this;
-          self.mode = ViewModes.view;
-          $scope.ViewModes = ViewModes;
-          $scope.box = {};
+    /** Defines the ArticleView module, and registers it with the module, which contains the View in
+     *     an HTML file (/js/blog-admin-app/article-view/article-view.template.html), with the associated Controller in
+     *     this file. This controller & view also manages creation, editing, and deletion of articles, which includes
+     *     altering which category they belong to.
+     * - $sanitize is used to allow injecting HTML into the page via the ng-bind-html tag.
+     * - $window is used to redirect the page to 'admin#!/article-list' after successfully deleting the article
+     *     currently being viewed.
+     * - $scope is used to make available a few functions so the view can indicate to the controller that the article
+     *     has been edited and should be saved, or that editing should cease/begin, etc.
+     * - $routeParams is used to access the ID of the article which is to be displayed.
+     * - ArticleIndex is used to access the articles from the server, and acts like a REST API with a GET function.
+     * - ServerConfig is used to allow access to the app-wide constant value of the base-url of the server.
+     * - ViewModes is used as a simple enum indicating which state of viewing is currently happening: whether the
+     *     article is being edited, or simply viewed without text areas on screen.
+     * - Category is used to access the categories from the server, and acts like a REST API with a GET function.
+     */
+    angular.module('articleView').component('articleView', {
+        templateUrl: '/js/blog-admin-app/article-view/article-view.template.html',
+        controller: ['$sanitize', '$window', '$scope', '$routeParams', 'ArticleIndex', 'ServerConfig', 'ViewModes', 'Category',
+            function articleViewController($sanitize, $window, $scope, $routeParams, ArticleIndex, ServerConfig, ViewModes, Category) {
+                var self = this;
+                self.mode = ViewModes.view;
+                $scope.ViewModes = ViewModes;
+                $scope.box = {};
 
-          self.articleId = $routeParams.articleId;
-          ArticleIndex.get(function successCallback (articles) {
-            // Filter out all but the article we want.
-            var article = articles.filter(function (article, n, list) {
-              return article._id === self.articleId;
-            })[0];
+                self.articleId = $routeParams.articleId;
+                ArticleIndex.get(function successCallback(articles) {
+                    /* Filter out all but the article we want; viz. the unique article with the desired ID. */
+                    var article = articles.filter(function (article) {
+                        return article._id === self.articleId;
+                    })[0];
 
-            var category = article.category;
+                    var category = article.category;
 
-            self.similarArticles = articles.filter(function (article, n, list) {
-              return category === article.category && article._id != self.articleId;
-            });
+                    /* Record for use in the View (in the 'Similar' side area) all other articles within the same
+                     * category.
+                     */
+                    self.similarArticles = articles.filter(function (article) {
+                        return category === article.category && article._id != self.articleId;
+                    });
 
-            // Make a deep copy of the article to restore back to.
-            self.article = JSON.parse(JSON.stringify(article));
-            $scope.box.article = article;
+                    /* Make a deep copy of the article to restore back to. */
+                    self.article = JSON.parse(JSON.stringify(article));
+                    $scope.box.article = article;
 
-            var categoryName = article.category;
-            if (!categoryName) {
-              categoryName = 'Miscellany';
-            }
+                    /* Get a list of all categories from the server. */
+                    Category.get(function successCallback(categories) {
+                        self.categoryList = categories;
+                        /* Having saved the list of all categories for use in the View (in the 'Categories' side area)
+                         * select the category corresponding to the currently viewed article.
+                         */
+                        for (var n in categories) {
+                            if (categories[n].name === self.article.category) {
+                                /* For reference, make a separate copy of the category. */
+                                self.category = JSON.parse(JSON.stringify(categories[n]));
+                                $scope.box.category = categories[n];
+                                break;
+                            }
+                        }
+                    });
+                });
 
-            // Get a list of all categories from the server.
-            Category.get(function successCallback (categories) {
-                self.categoryList = categories;
-                for (var n in categories) {
-                  if (categories[n].name === self.article.category) {
-                    // For reference, make a separate copy of the category.
-                    self.category = JSON.parse(JSON.stringify(categories[n]));
-                    $scope.box.category = categories[n];
-                    break;
-                  }
+                /* Save the base-url in the Controller, so it can be accessed throughout the View and Controller. */
+                self.serverBaseUrl = ServerConfig.baseUrl;
+
+                /*
+                 Operations called from within the view
+                 */
+
+                $scope.enterEditMode = function enterEditMode() {
+                    self.mode = ViewModes.edit;
+                };
+
+                $scope.cancelEdits = function cancelEdits() {
+                    self.mode = ViewModes.view;
+                    /* Copy the restore version back to the viewed version. */
+                    $scope.box.article = JSON.parse(JSON.stringify(self.article));
+                };
+
+                /* Confirm the admin would like to save the updated category */
+                $scope.saveCategoryChange = function saveCategoryChange(category) {
+                    confirmBefore('change article category to ' + category.name, function confirmedCallback() {
+                        $scope.box.article.category = category.name;
+                        saveArticle($scope.box.article);
+                    }, function elseCallback() {
+                        $scope.box.article.category = self.article.category;
+                    });
                 }
-              });
-          });
 
-          self.serverBaseUrl = ServerConfig.baseUrl;
+                /* Confirm the admin would like to save the article */
+                $scope.saveArticleUpdates = function saveArticleUpdates(article) {
+                    confirmBefore('save article ' + article._id, function confirmedCallback() {
+                        saveArticle(article);
+                    }, function elseCallback() {
+                    });
+                }
 
-          /*
-            Operations called from within the view
-          */
+                /* Confirm the admin would like to delete the article */
+                $scope.deleteCurrentArticle = function deleteCurrentArticle() {
+                    confirmBefore('delete article ' + self.articleId, function confirmedCallback() {
+                        deleteArticle(self);
+                    }, function elseCallback() {
+                    });
+                }
 
-          $scope.saveCategoryChange = function saveCategoryChange (category) {
-            confirmBeforeSavingCategoryChange(category);
-          };
+                /* Confirm the desired operation, then execute the callback if affirmed. */
+                function confirmBefore(description, confirmedCallback, elseCallback) {
+                    if (confirm('Are you sure you would like to ' + description + '?')) {
+                        confirmedCallback();
+                    } else {
+                        elseCallback();
+                    }
+                }
 
-          $scope.saveArticleUpdates = function saveArticleUpdates (article) {
-            confirmBeforeSavingArticle(article);
-          };
+                /* PUT updates to the current article, saving the new data on the server, and refreshing the local data
+                 * store.
+                 */
+                function saveArticle(article) {
+                    /* If the article is (or will be) private, check to see if all the articles in it's category are
+                     * private; if they are, make sure that the category is private.
+                     */
+                    if (article.private && self.similarArticles.filter(function (a) {
+                            return !a.private;
+                        }).length == 0) {
+                        if (!self.category.private) {
+                            self.category.private = true;
+                            Category.updateCategory(self.category._id, self.category,
+                                function successCallback(response) {
+                                    /* Ensure this article's category reflects that on the server. */
+                                    self.category = JSON.parse(JSON.stringify(response.data));
+                                }, function errorCallback(response) {
+                                    console.error(response);
+                                }
+                            );
+                        }
+                    }
 
-          $scope.saveCommentUpdates = function saveCommentUpdates (comment) {
-            confirmBeforeSavingComment(comment);
-          };
+                    ArticleIndex.updateArticle(article,
+                        function successCallback(response) {
+                            /* Make a copy of the new article */
+                            self.article = JSON.parse(JSON.stringify(response.data.Success));
+                            /* And update the view to match the object saved and returned by the server. */
+                            $scope.box.article = response.data.Success;
+                            ArticleIndex.refresh(function () {
+                                self.mode = ViewModes.view;
+                            });
+                        }, function errorCallback(response) {
+                            console.error('Error: ', response);
+                        });
+                }
 
-          $scope.enterEditMode = function enterEditMode () {
-            self.mode = ViewModes.edit;
-          };
-
-          $scope.cancelEdits = function cancelEdits () {
-            self.mode = ViewModes.view;
-            // Copy the restore version back to the viewed version.
-            $scope.box.article = JSON.parse(JSON.stringify(self.article));
-          };
-
-          $scope.deleteCurrentArticle = function deleteCurrentArticle () {
-            confirmBeforeDeletingArticle();
-          };
-
-          $scope.deleteCommentById = function deleteCommentById (id) {
-            confirmBeforeDeletingComment(id);
-          };
-
-          /*
-            Helper functions for verification
-          */
-
-          /* Confirm the admin would like to save the updated category */
-          function confirmBeforeSavingCategoryChange(category) {
-            confirmBefore('change article category to ' + category.name, function confirmedCallback() {
-              $scope.box.article.category = category.name;
-              saveArticle($scope.box.article);
-            }, function elseCallback () {
-              $scope.box.article.category = self.article.category;
-            });
-          }
-
-          /* Confirm the admin would like to save a comment */
-          function confirmBeforeSavingComment(comment) {
-            confirmBefore('save comment ' + comment._id, function confirmedCallback() {
-              saveComment(comment);
-            }, function elseCallback() {
-            });
-          }
-
-          /* Confirm the admin would like to delete a comment */
-          function confirmBeforeDeletingComment(commentId) {
-            confirmBefore('delete comment ' + commentId, function confirmedCallback() {
-              deleteComment(commentId);
-            }, function elseCallback() {
-            });
-          }
-
-          /* Confirm the admin would like to save the article */
-          function confirmBeforeSavingArticle(article) {
-            confirmBefore('save article ' + article._id, function confirmedCallback() {
-              saveArticle(article);
-            }, function elseCallback() {
-            });
-          }
-
-          /* Confirm the admin would like to delete the article */
-          function confirmBeforeDeletingArticle() {
-            confirmBefore('delete article ' + self.articleId, function confirmedCallback() {
-              deleteArticle(self);
-            }, function elseCallback() {
-            });
-          }
-
-          /* Confirm the desired operation, then execute the callback if affirmed. */
-          function confirmBefore(description, confirmedCallback, elseCallback) {
-            if (confirm('Are you sure you would like to ' + description + '?')) {
-              confirmedCallback();
-            } else {
-              elseCallback();
+                /* DELETE the current article, by ID. */
+                function deleteArticle() {
+                    ArticleIndex.deleteArticle(self.article._id, function successCallback(response) {
+                            /* If successful, just reroute to the article & category list, making sure to refresh the
+                             * data model for both categories and articles before doing so; admins should always see
+                             * the most up-to-date information
+                             */
+                            ArticleIndex.refresh(function () {
+                                Category.refresh(function () {
+                                    $window.location = '/admin#!/article-list';
+                                });
+                            });
+                        }, function errorCallback(response) {
+                            /* If an error occurs, display it and don't navigate away from the page,
+                             * so any work can be saved locally.
+                             */
+                            console.error(response);
+                        }
+                    );
+                }
             }
-          }
-
-          /*
-            SAVE operations:
-              - PUT a comment by ID from the current article
-          */
-
-          /* PUT updates to the current article, saving the new data on the server,
-                  and refreshing the local data store. */
-          function saveArticle(article) {
-            /* If the article is (or will be) private, check to see if all
-                the articles in it's category are private; if they are, make
-                sure that the category is private. */
-            if (article.private &&
-                self.similarArticles.filter(function (a) { return !a.private; })
-                  .length == 0) {
-              if (!self.category.private) {
-                self.category.private = true;
-                Category.updateCategory(self.category._id, self.category,
-                  function successCallback (response) {
-                    // Ensure this article's category reflects that on the server.
-                    self.category = JSON.parse(JSON.stringify(response.data));
-                  }, function errorCallback (response) {
-                    console.error(response);
-                  }
-                );
-              }
-            }
-
-            ArticleIndex.updateArticle(article,
-              function successCallback (response) {
-                // Make a copy of the new article
-                self.article = JSON.parse(JSON.stringify(response.data.Success));
-                // And update the view to match the object saved and returned by the server.
-                $scope.box.article = response.data.Success;
-                ArticleIndex.refresh(function () {
-                  self.mode = ViewModes.view;
-                });
-              }, function errorCallback (response) {
-                console.log('Error: ', response);
-              });
-          }
-
-          /*
-            DELETE operations:
-              - DELETE the current article, by ID.
-          */
-
-          /* DELETE the current article, by ID. */
-          function deleteArticle() {
-            ArticleIndex.deleteArticle(self.article._id,
-              function successCallback(response) {
-                // If successful, just reroute to the article & category list.
-                ArticleIndex.refresh(function () {
-                  Category.refresh(function () {
-                    $window.location = '/admin#!/article-list';
-                  });
-                });
-              }, function errorCallback(response) {
-                // If an error occurrs, display it and don't navigate away from the page,
-                //    so any work can be saved locally.
-                console.log(response);
-              }
-            );
-          }
-        }
-      ]
+        ]
     });
 })();
--- public/js/blog-admin-app/article-view/article-view.module.js
+++ public/js/blog-admin-app/article-view/article-view.module.js
@@ -1,10 +1,16 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	// Define the 'articleView' module
-	angular.module('articleView', [
-		'ngSanitize',
-		'core.articleIndex',
-		'core.category'
-	]);
+    /** Define the 'articleView' module, which encapsulates the View and Controller for pages which view individual
+     *     articles, by ID. This module also allows the creation, editing, and deletion of categories, which includes
+     *     making them public or private.
+     * - ngSanitize is needed in order to allow injecting HTML into the page via ng-bind-html
+     * - core.articleIndex is needed in order to access articles from the server, which can then be viewed by ID.
+     * - core.category is needed in order to access categories from the server, which can then be viewed by name.
+     */
+    angular.module('articleView', [
+        'ngSanitize',
+        'core.articleIndex',
+        'core.category'
+    ]);
 })();
