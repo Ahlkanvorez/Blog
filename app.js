e8b164577a976285b7153f307c7835c226914cd1
Robert Mitchell <robert.mitchell36@gmail.com>
2016-11-08 18:59:57 -0600

    Partially commented app.js

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-// Database
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+/* Database requirements and initialization */
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/blog');
+const db = mongoose.connection;
 
-var blogDatabase = require('./data/blog-database.js');
-var db = mongoose.connection;
+/* Route files */
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const blog = require('./routes/blog')
 
-// Routes
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var blog = require('./routes/blog')
+const app = express();
 
-var app = express();
-
-// view engine setup
+/* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/* uncomment the following after placing your favicon in '/public':
+ * app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+ */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('Ahlkanvorez')); // Initialize with a secret.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Configure the session
+/* Configure the session
+ * TODO: Review the session settings.
+ */
 var sess = {
   secret : 'Ahlkanvorez',
   resave : false,
-  cookie : { secure : false } // TODO: Make work with secure : true.
+  cookie : { secure : false } // TODO: Make it work with secure : true.
 };
 
 if (app.get('env') === 'production') {
@@ -47,44 +47,43 @@ if (app.get('env') === 'production') {
 
 app.use(session(sess));
 
-// Make the database accessible to the router
+/* Make the database accessible to the router */
 app.use(function(req, res, next) {
     req.db = db;
     next();
 });
 
+/* Register the routers for this app with the server. */
 app.use('/', routes);
 app.use('/users', users);
 app.use('/blog', blog);
 
-// catch 404 and forward to error handler
+/* catch 404 and forward to error handler */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+/* Error handlers */
 
-// development error handler
-// will print stacktrace
+/* Development error handler will print stacktrace */
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+      message : err.message,
+      error : err
     });
   });
 }
 
-// production error handler
-// no stack-traces leaked to user
+/* production error handler no stack-traces leaked to user */
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
-    error: {}
+    message : err.message,
+    error : {}
   });
 });
 
