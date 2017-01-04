8f0efbbed43452065b88dbe6c06b6dad24728ca4
Ahlkanvorez <robert.mitchell36@gmail.com>
2017-01-04 23:32:30 +0000

    Fixed horrible evil bug with the load order of the JS files in the Jade document by using GulpJS to minify and concatenate all the scripts into one file and simply include that one file in the page. Also corrected a typo in article-view.template.html where a closing parenthesis was missing.

--- public/js/blog-app/app.constants.js
+++ public/js/blog-app/app.constants.js
@@ -1,9 +1,9 @@
 (function () {
-	'use strict';
+    'use strict';
 
     /** Defines some constants used throughout the whole app, such as the base URL of the website. */
-	angular.module('blogApp').
-		constant('ServerConfig', {
-			baseUrl : 'https://www.hrodebert.me' // TODO: Change to url of server before official release.
-		});
+    angular.module('blogApp').
+	constant('ServerConfig', {
+	    baseUrl : 'https://www.hrodebert.me'
+	});
 })();
