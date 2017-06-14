e75d1d2a1b49ce7428f08740dc2b359851d47ac7
Ahlkanvorez <robert.mitchell36@gmail.com>
2017-06-14 19:07:10 +0000

    Revert "Returned to Promises instead of async/await, and updated the startup script to handle the server if already running."
    This reverts commit 1d6192288d0f0a3a1c466c95c354db439c08b48d.

--- app.js
+++ app.js
@@ -26,10 +26,6 @@ const app = express();
 const helmet = require('helmet');
 app.use(helmet());
 
-/* Provides automatic performance boosts. */
-const compression = require('compression');
-app.use(compression());
-
 /* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -84,6 +80,13 @@ app.get('/robots.txt', function (req, res) {
     res.send("User-agent: *\nDisallow: /admin/\nDisallow: /users/\nDisallow: /blog/");
 });
 
+/* Log the IP information for each request */
+app.use(function(req, res, next) {
+    var ip_info = get_ip(req);
+    console.log(ip_info);
+    next();
+});
+
 /* Make the database accessible to the router */
 app.use(function(req, res, next) {
     req.db = db;
@@ -95,7 +98,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/blog', blog);
 
-/* Log errors using winston. */
+/* Log errors using winston */
 app.use(expressWinston.errorLogger({
     transports: [
         new winston.transports.Console({
@@ -123,14 +126,6 @@ if (app.get('env') === 'development') {
       error : err
     });
   });
-
-  /* Log the IP information for each request */
-  app.use(function(req, res, next) {
-    var ip_info = get_ip(req);
-    console.log(ip_info);
-    next();
-  });
-
 }
 
 /* production error handler no stack-traces leaked to user */
@@ -138,7 +133,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message : err.message,
-    error : {}
+    error : {},
   });
 });
 
