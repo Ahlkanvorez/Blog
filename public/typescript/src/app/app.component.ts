23018171d7fc8214122c618263af022ad2e58af0
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-15 11:27:12 -0500

    Added a basic Article class to the app.component

--- public/typescript/src/app/app.component.ts
+++ public/typescript/src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+export class Article {
+  title: string;
+  author: { name: string; email: string; };
+  date: Date;
+  category: { type: string };
+  content: string;
+  sticky: boolean;
+  image: string;
+  image_dimensions: { width: number, height: number };
+}
+
 @Component({
   selector: 'blog',
   template: `<h1>Hello {{name}}</h1>`,
