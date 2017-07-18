632c97207edc8826d3e59935f07956d91c89e34d
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-18 14:15:23 -0500

    Added a call to get category info for the article-view component and display it in the view.

--- public/typescript/src/app/articles/article-view.component.ts
+++ public/typescript/src/app/articles/article-view.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit} from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Location } from '@angular/common';
 import { Article } from "./article";
+import { Category } from "../categories/category";
 import { ArticleService } from './article.service';
+import { CategoryService } from "../categories/category.service";
 import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -11,13 +12,23 @@ import 'rxjs/add/operator/switchMap';
 })
 export class ArticleViewComponent implements OnInit {
   @Input() article: Article;
+  @Input() category: Category;
 
-  constructor (private articleService: ArticleService, private route: ActivatedRoute) {}
+  constructor (private articleService: ArticleService,
+               private categoryService: CategoryService,
+               private route: ActivatedRoute) {}
 
   ngOnInit (): void {
     // Extract the name from the parameters, and populate the article member accordingly from the article service.
     this.route.paramMap
       .switchMap((params: ParamMap) => this.articleService.getArticle(params.get('title')))
-      .subscribe(article => this.article = article);
+      .subscribe(article => {
+        this.article = article;
+
+        // Once the article has been gathered, grab the appropriate category.
+        this.route.paramMap
+          .switchMap((params: ParamMap) => this.categoryService.getCategory(this.article.category))
+          .subscribe(category => this.category = category);
+      });
   }
 }
