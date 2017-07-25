4456073f9b2083684790b4437dfd92f4dd2b2ac2
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-25 13:45:32 -0500

    Enabled production mode.

--- public/Angular2/src/main.ts
+++ public/Angular2/src/main.ts
@@ -5,5 +5,8 @@ import { AppModule } from './app/app.module';
 // To prevent errors due to missing dependencies after webpack build.
 import 'zone.js';
 import 'reflect-metadata';
+import { enableProdMode } from "@angular/core";
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
