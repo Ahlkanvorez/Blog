5070ae328db004afa421fcba263099af10f5d5ad
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-25 10:41:21 -0500

    Added import statements for dependencies otherwise not referenced in main.ts

--- public/Angular2/src/main.ts
+++ public/Angular2/src/main.ts
@@ -2,4 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// To prevent errors due to missing dependencies after webpack build.
+import 'zone.js';
+import 'reflect-metadata';
+
 platformBrowserDynamic().bootstrapModule(AppModule);
