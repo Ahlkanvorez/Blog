bb01c6b163291a38a81e349d750906ad9662d252
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-25 14:38:22 -0500

    Added article cache to reduce number of server requests.

--- public/Angular2/src/app/articles/article.service.ts
+++ public/Angular2/src/app/articles/article.service.ts
@@ -11,14 +11,22 @@ export class ArticleService {
   // private articlesUrl = 'api/articles';
   private articlesUrl = 'https://www.hrodebert.com/blog';
 
+  private cache: Article[];
+
   constructor (private http: Http) {}
 
   getArticles (): Promise<Article[]> {
     // Development request
     // return this.http.get(this.articlesUrl)
+    if (this.cache && this.cache.length > 0) {
+      return Promise.resolve(this.cache);
+    }
     return this.http.get(`${this.articlesUrl}/article-list`)
       .toPromise()
-      .then((res: any) => JSON.parse(res._body) as Article[])
+      .then((res: any) => {
+        this.cache = JSON.parse(res._body) as Article[];
+        return this.cache;
+      })
       .catch(this.handleError);
   }
 
@@ -26,6 +34,9 @@ export class ArticleService {
     // Note: The annoying in-memory-data service uses the pattern :base/:collectionName/:id?, so the title parameter has
     // to be passed via ?title=___. This is annoying, and should not be reflected in the server-side REST api.
     // return this.http.get(`${this.articlesUrl}/?title=${title}`)
+    if (this.cache && this.cache.length > 0) {
+      return Promise.resolve(this.cache.find(article => article.title === title));
+    }
     return this.http.get(`${this.articlesUrl}/get-article/title/${title}`)
       .toPromise()
       .then((res: any) => JSON.parse(res._body) as Article)
