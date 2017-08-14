82be9fea1ed786e95f697d88f3917d4065550320
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-14 10:02:31 -0500

    Corrected indentation.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const expressWinston = require('express-winston'); /* logging */
+const expressWinston = require('express-winston'); // logging
 const winston = require('winston');
 const get_ip = require('ipware')().get_ip;
 
@@ -131,39 +131,38 @@ app.use(expressWinston.errorLogger({
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Error handlers
 
 // Development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message : err.message,
-      error : err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message : err.message,
+            error : err
+        });
     });
-  });
-
-  // Log the IP information for each request
-  app.use(function(req, res, next) {
-    var ip_info = get_ip(req);
-    console.log(ip_info);
-    next();
-  });
-
+  
+    // Log the IP information for each request
+    app.use(function(req, res, next) {
+        var ip_info = get_ip(req);
+        console.log(ip_info);
+        next();
+    });
 }
 
 // production error handler no stack-traces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message : err.message,
-    error : {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message : err.message,
+        error : {}
+    });
 });
 
 
