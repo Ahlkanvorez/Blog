368f48c644eae3121360437f1387789377fc28d1
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 12:23:52 -0500

    Made the files for the angularJS public article-index conform to an 80 character max line length.

--- public/angularJS/blog-app/core/article-index/article-index.service.js
+++ public/angularJS/blog-app/core/article-index/article-index.service.js
@@ -1,29 +1,40 @@
 (function () {
     'use strict';
 
-    /** Defines the ArticleIndex service, which manages all interaction with the server concerning Article objects.
-     *  The REST API presented by this service only allows HTTP GET operations, since the user will never need to, and
-     *     is not allowed to, perform any other options.
+    /** Defines the ArticleIndex service, which manages all interaction with the
+     * server concerning Article objects. The REST API presented by this service
+     * only allows HTTP GET operations, since the user will never need to, and
+     * is not allowed to, perform any other options.
      *
-     *  - get will return all the articles on the server, but will only access the server once, caching the response
-     *        in memory, and serving that data when re-requested.
+     *  - get will return all the articles on the server, but will only access
+     *    the server once, caching the response in memory, and serving that data
+     *    when re-requested.
      *
      * Requirements:
-     *  - $http is required in order to send HTTP requests to the server (in this case, only GET).
-     *  - ServerConfig is required in order to access the base URL of the server, which is only recorded in that one
-     *      location, to prevent possible bugs if the server url ever changes.
+     *  - $http is required in order to send HTTP requests to the server (in
+     *    this case, only GET).
+     *  - ServerConfig is required in order to access the base URL of the
+     *    server, which is only recorded in that one location, to prevent
+     *    possible bugs if the server url ever changes.
      *
+     * @param {object} $http the AngularJS $http module
+     * @param {object} ServerConfig app constants with server URL info.
+     * 
+     * @return {object} an abstraction of a REST API for article objects.
      */
-    angular.module('core.articleIndex').factory('ArticleIndex', ['$http', 'ServerConfig',
-        function ArticleIndexService($http, ServerConfig) {
-            /* Record the promise of an HTTP GET request for the category list. */
-            var promise = $http.get(ServerConfig.baseUrl + '/blog/article-list');
+    angular.module('core.articleIndex')
+           .factory('ArticleIndex', [ '$http', 'ServerConfig',
+        function ArticleIndexService ($http, ServerConfig) {
+            // Record the promise of an HTTP GET request for the category list.
+            const promise = $http.get(ServerConfig.baseUrl
+                + '/blog/article-list');
 
             return {
                 get: function (callback) {
-                    /* Pass the given callback to the promise for use when the HTTP GET request is successful,
-                     so that the caller interacts with this as if they were just making an HTTP GET request each
-                     time get(callback) is called. */
+                    // Pass the given callback to the promise for use when the
+                    // HTTP GET request is successful, so that the caller
+                    // interacts with this as if they were just making an HTTP
+                    // GET request each time get(callback) is called.
                     promise.success(callback);
                 }
             };
