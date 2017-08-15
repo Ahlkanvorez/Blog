552064f0641c95c10c174a9795fb590fa3dda2fb
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 11:25:23 -0500

    Made the core article-index files in the admin app conform to 80 character max line lengths.

--- public/angularJS/blog-admin-app/core/article-index/article-index.service.js
+++ public/angularJS/blog-admin-app/core/article-index/article-index.service.js
@@ -1,64 +1,86 @@
 (function () {
     'use strict';
 
-    /** Defines the ArticleIndex service, which manages all interaction with the server concerning Article objects.
-     *  The REST API presented by this service allows GET, POST, PUT, and DELETE operations, since the admin will need
-     *     to be able to view, create, edit, and delete articles.
+    /** Defines the ArticleIndex service, which manages all interaction with the
+     * server concerning Article objects.
+     * The REST API presented by this service allows GET, POST, PUT, and DELETE
+     * operations, since the admin will need to be able to view, create, edit,
+     * and delete articles.
      *
-     *  - get will return all the articles on the server, but will only access the server once, caching the response in
-     *        memory, and serving that data when re-requested.
-     *  - refresh will reload the local data from the server, by creating a new HTTP request and saving a new promise.
+     *  - get will return all the articles on the server, but will only access
+     *    the server once, caching the response in memory, and serving that data
+     *    when re-requested.
+     *  - refresh will reload the local data from the server, by creating a new
+     *    HTTP request and saving a new promise.
      *  - createArticle creates a new article via a POST request.
-     *  - updateArticle allows changes to an existing article to be made by ID, via a PUT request.
-     *  - deleteArticle removes an article from the database by ID, via a DELETE request.
+     *  - updateArticle allows changes to an existing article to be made by ID,
+     *    via a PUT request.
+     *  - deleteArticle removes an article from the database by ID, via a DELETE
+     *    request.
      *
      * Requirements:
      *  - $http is required in order to send HTTP requests to the server.
-     *  - ServerConfig is required in order to access the base URL of the server, which is only recorded in that one
-     *      location, to prevent possible bugs if the server url ever changes.
+     *  - ServerConfig is required in order to access the base URL of the
+     *    server, which is only recorded in that one location, to prevent
+     *    possible bugs if the server url ever changes.
      *
+     * 
+     * @param {object} $http the angularJS $http module.
+     * @param {object} ServerConfig app constants to inform the service of the
+     *                              target url
+     * 
+     * @return {object} an object abstracting a REST API for articles.
      */
-    angular.module('core.articleIndex').factory('ArticleIndex', ['$http', 'ServerConfig',
-        function ArticleIndexService($http, ServerConfig) {
-            var allArticleListUrl = ServerConfig.baseUrl + '/blog/all-article-list';
-            /* Record the promise for article objects */
-            var promise = $http.get(allArticleListUrl);
+    angular.module('core.articleIndex')
+           .factory('ArticleIndex', [ '$http', 'ServerConfig',
+        function ArticleIndexService ($http, ServerConfig) {
+            const allArticleListUrl = ServerConfig.baseUrl
+                + '/blog/all-article-list';
+            // Record the promise for article objects
+            let promise = $http.get(allArticleListUrl);
 
             return {
                 get: function (callback) {
-                    /* Feed the promise the given callback, so the data can be retrieved as if it were a fresh request
-                     * each time.
-                     */
+                    // Feed the promise the given callback, so the data can be
+                    // retrieved as if it were a fresh request each time.
                     promise.success(callback);
                 },
 
                 refresh: function (callback) {
-                    /* Create a new promise and feed that the callback, so that the data is actually reloaded from the
-                     * server, and then appears to be a fresh request on each subsequent access.
-                     */
+                    // Create a new promise and feed that the callback, so that
+                    // the data is actually reloaded from the server, and then
+                    // appears to be a fresh request on each subsequent access.
                     promise = $http.get(allArticleListUrl);
                     promise.success(callback);
                 },
 
-                createArticle: function (category, successCallback, errorCallback) {
+                createArticle: function (category, successCallback,
+                                         errorCallback) {
                     $http({
                         method: 'POST',
                         url: ServerConfig.baseUrl + '/blog/post-article/',
                         data: {
-                            "title": "Title",
-                            "author": {"name": "Author name", "email": "Author email"},
-                            "content": "<p>This is some content.</p>",
-                            "category": ((!category || category === 'Everything') ? 'Miscellany' : category),
-                            "date": new Date().toString(),
-                            "private": true
+                            title: 'Title',
+                            author: {
+                                name: 'Author name',
+                                email: 'Author email'
+                            },
+                            content: '<p>This is some content.</p>',
+                            category: ((!category || category === 'Everything')
+                                        ? 'Miscellany'
+                                        : category),
+                            date: new Date().toString(),
+                            private: true
                         }
                     }).then(successCallback, errorCallback);
                 },
 
-                updateArticle: function (article, successCallback, errorCallback) {
+                updateArticle: function (article, successCallback,
+                                         errorCallback) {
                     $http({
                         method: 'PUT',
-                        url: ServerConfig.baseUrl + '/blog/update-article/' + article._id,
+                        url: ServerConfig.baseUrl + '/blog/update-article/'
+                            + article._id,
                         data: article
                     }).then(successCallback, errorCallback);
                 },
