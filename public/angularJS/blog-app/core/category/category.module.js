87edf8dca574dfb38941ba4d8efe86c98cbce4f1
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 12:26:27 -0500

    Made the files for the angularJS public category service conform to an 80 character max line length.

--- public/angularJS/blog-app/core/category/category.module.js
+++ public/angularJS/blog-app/core/category/category.module.js
@@ -1,8 +1,10 @@
 (function () {
     'use strict';
 
-    /** Defines the Category module, which handles all interaction with the server for Category objects.
-     * - ngResource is required in order to interact with the server's REST API, by sending HTTP requests.
+    /** Defines the Category module, which handles all interaction with the
+     * server for Category objects.
+     * - ngResource is required in order to interact with the server's REST API,
+     *   by sending HTTP requests.
      */
-    angular.module('core.category', ['ngResource']);
+    angular.module('core.category', [ 'ngResource' ]);
 })();
--- public/angularJS/blog-app/core/category/category.service.js
+++ public/angularJS/blog-app/core/category/category.service.js
@@ -1,28 +1,40 @@
 (function () {
     'use strict';
 
-    /** Defines the Category service, which manages all interaction with the server concerning Category objects.
-     *  The REST API presented by this service only allows HTTP GET operations, since the user will never need to, and
-     *     is not allowed to, perform any other options.
+    /** Defines the Category service, which manages all interaction with the
+     * server concerning Category objects. The REST API presented by this
+     * service only allows HTTP GET operations, since the user will never need
+     * to, and is not allowed to, perform any other options.
      *
-     *  - get will return all the categories on the server, but will only access the server once, caching the response
-     *        in memory, and serving that data when re-requested.
+     * - get will return all the categories on the server, but will only access
+     *   the server once, caching the response in memory, and serving that data
+     *   when re-requested.
      *
      * Requirements:
-     *  - $http is required in order to send HTTP requests to the server (in this case, only GET).
-     *  - ServerConfig is required in order to access the base URL of the server, which is only recorded in that one
-     *      location, to prevent possible bugs if the server url ever changes.
+     *  - $http is required in order to send HTTP requests to the server (in
+     *    this case, only GET).
+     *  - ServerConfig is required in order to access the base URL of the
+     *    server, which is only recorded in that one location, to prevent
+     *    possible bugs if the server url ever changes.
+     * 
+     * @param {object} $http the angularJS http module
+     * @param {object} ServerConfig app constants with server URL info
+     * 
+     * @return {object} an abstraction of a REST api for categories.
      */
-    angular.module('core.category').factory('Category', ['$http', 'ServerConfig',
+    angular.module('core.category')
+           .factory('Category', [ '$http', 'ServerConfig',
         function ($http, ServerConfig) {
-            /* Record the promise of an HTTP GET request for the category list. */
-            const promise = $http.get(ServerConfig.baseUrl + '/blog/category-list');
+            // Record the promise of an HTTP GET request for the category list.
+            const promise = $http.get(ServerConfig.baseUrl
+                + '/blog/category-list');
 
             return {
                 get: function (callback) {
-                    /* Pass the given callback to the promise for use when the HTTP GET request is successful,
-                     so that the caller interacts with this as if they were just making an HTTP GET request each
-                     time get(callback) is called. */
+                    // Pass the given callback to the promise for use when the
+                    // HTTP GET request is successful, so that the caller
+                    // interacts with this as if they were just making an
+                    // HTTP GET request each time get(callback) is called.
                     promise.success(callback);
                 }
             };
