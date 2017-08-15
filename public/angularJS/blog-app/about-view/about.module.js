a74127f6732955d19b6457eec512e9f453583966
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 11:51:46 -0500

    Made the public angularJS about view conform to an 80 character max line length.

--- public/angularJS/blog-app/about-view/about.module.js
+++ public/angularJS/blog-app/about-view/about.module.js
@@ -1,8 +1,10 @@
 (function () {
     'use strict';
 
-    /** Defines the 'aboutView' module, which encapsulates the View and Controller for the static About page.
-     * The About view is a static HTML page, and consequently this module has no dependencies and an empty Controller.
+    /** Defines the 'aboutView' module, which encapsulates the View and
+     * Controller for the static About page.
+     * The About view is a static HTML page, and consequently this module has no
+     * dependencies and an empty Controller.
      */
     angular.module('aboutView', []);
 })();
