a1bf9e0e5d788c87335e0dba30b8f94bde030bf7
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 16:54:24 -0500

    Improved some code styling and fixed a bug where requesting a non-existant article could cause an error in meta-fetching.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 (function () {
     const express = require('express');
-    const path = require('path');
     const blogDatabase = require('../data/blog-database');
     const checkAuth = require('./auth').checkAuth;
     const router = express.Router();
@@ -10,29 +9,29 @@
 
     const angular2Template = 'index.html';
 
-    const default_meta = {
-            title : 'Blog | Robert Mitchell',
-            description : "I write to investigate the Good, the Beautiful, the "
-                + "Simple, the Sublime, and the Just, in order to better "
-                + "understand what wisdom is, what a good education is, and "
-                + "what a good life is.",
-            url : 'https://www.hrodebert.com/',
-            type : 'blog',
-            image : 'http://www.hrodebert.com/img/favicon.png',
-            image_secure_url : 'https://www.hrodebert.com/img/favicon.png',
-            image_width : '556',
-            image_height : '554',
-            twitter_site : '@RobertMitchel_l',
-            twitter_card : 'summary'
+    const defaultMeta = {
+            title: 'Blog | Robert Mitchell',
+            description: 'I write to investigate the Good, the Beautiful, the '
+                + 'Simple, the Sublime, and the Just, in order to better '
+                + 'understand what wisdom is, what a good education is, and '
+                + 'what a good life is.',
+            url: 'https://www.hrodebert.com/',
+            type: 'blog',
+            image: 'http://www.hrodebert.com/img/favicon.png',
+            image_secure_url: 'https://www.hrodebert.com/img/favicon.png',
+            image_width: '556',
+            image_height: '554',
+            twitter_site: '@RobertMitchel_l',
+            twitter_card: 'summary'
     };
 
     const getMetaForArticle = function (articleName, callback) {
         const correctedName = articleName.split('-').join(' ');
         Article.find().public()
                 .byTitle(correctedName).exec(function (err, articles) {
-            if (articles === []) {
+            if (articles.length === 0) {
                 // No such article exists, so send the default meta for the site
-                callback(default_meta);
+                callback(defaultMeta);
                 return;
             }
             const article = articles[0];
@@ -40,7 +39,7 @@
             // any wrapping HTML tags trimmed.
             // TODO: Determine whether HTML is supported in Open Graph meta tags
             //       and Twitter card meta tags.
-            let snippet = new RegExp("^([^.!?。]+.)").exec(article.content)[0];
+            let snippet = new RegExp('^([^.!?。]+.)').exec(article.content)[0];
             if (snippet.indexOf('<p>') === 0) {
                 snippet = snippet.substring('<p>'.length);
             }
@@ -48,23 +47,23 @@
                 snippet = snippet.substring('<blockquote>'.length);
             }
             callback({
-                title : article.title
+                title: article.title
                     + (article.category !== 'Miscellany'
                         ? ' | ' + article.category
                         : ''),
-                description : snippet.trim(),
-                url : 'https://www.hrodebert.com/articles/' + article.title,
-                type : 'blog',
-                image : 'http://www.hrodebert.com/'
+                description: snippet.trim(),
+                url: 'https://www.hrodebert.com/articles/' + article.title,
+                type: 'blog',
+                image: 'http://www.hrodebert.com/'
                     + (article.image || 'img/favicon.png'),
-                image_secure_url : 'https://www.hrodebert.com/'
+                image_secure_url: 'https://www.hrodebert.com/'
                     + (article.image || 'img/favicon.png'),
-                image_width : article.image_dimensions.width
-                    || default_meta.image_width,
-                image_height : article.image_dimensions.height
-                    || default_meta.image_height,
-                twitter_site : '@RobertMitchel_l',
-                twitter_card : 'summary'
+                image_width: article.image_dimensions.width
+                    || defaultMeta.image_width,
+                image_height: article.image_dimensions.height
+                    || defaultMeta.image_height,
+                twitter_site: '@RobertMitchel_l',
+                twitter_card: 'summary'
             });
         });
     };
@@ -77,9 +76,9 @@
                 .byName(categoryName).exec(function (err, categories) {
             if (categories === []) {
                 // No such category exists; send the default meta for the site.
-                console.log("Couldn't find the desired category:",
+                console.log('Couldn\'t find the desired category:',
                     categoryName);
-                callback(default_meta);
+                callback(defaultMeta);
             }
             const category = categories[0];
             let name = category.name;
@@ -87,20 +86,20 @@
                 name = 'Latest Articles';
             }
             callback({
-                title : name + ' | Robert Mitchell',
-                description : category.aboutAuthor,
-                url : "https://www.hrodebert.com/article-list/" + category.name,
-                type : 'blog',
-                image : 'http://www.hrodebert.com/'
+                title: name + ' | Robert Mitchell',
+                description: category.aboutAuthor,
+                url: 'https://www.hrodebert.com/article-list/' + category.name,
+                type: 'blog',
+                image: 'http://www.hrodebert.com/'
                     + (category.image || 'img/favicon.png'),
-                image_secure_url : 'https://www.hrodebert.com/'
+                image_secure_url: 'https://www.hrodebert.com/'
                     + (category.image || 'img/favicon.png'),
-                image_width : category.image_dimensions.width
-                    || default_meta.image_width,
-                image_height : category.image_dimensions.height
-                    || default_meta.image_height,
-                twitter_site : '@RobertMitchel_l',
-                twitter_card : 'summary'
+                image_width: category.image_dimensions.width
+                    || defaultMeta.image_width,
+                image_height: category.image_dimensions.height
+                    || defaultMeta.image_height,
+                twitter_site: '@RobertMitchel_l',
+                twitter_card: 'summary'
             });
         });
     };
@@ -111,30 +110,30 @@
     // and is a completely separate AngularJS app from that of the admin panel
     // site.
     router.get('/', function (req, res, next) {
-        res.render(angular2Template, default_meta);
+        res.render(angular2Template, defaultMeta);
     });
 
     // GET the about page.
     // Renders the about page with the proper meta tags for SEO.
     router.get('/about', function (req, res, next) {
         res.render(angular2Template, {
-            title : 'About | Robert Mitchell',
-            description : default_meta.description,
-            url : 'https://www.hrodebert.com/about',
-            type : 'blog',
-            image : 'http://www.hrodebert.com/img/Me.jpg',
-            image_secure_url : 'https://www.hrodebert.com/img/Me.jpg',
-            image_width : '638',
-            image_height : '638',
-            twitter_site : '@RobertMitchel_l',
-            twitter_card : 'summary'
+            title: 'About | Robert Mitchell',
+            description: defaultMeta.description,
+            url: 'https://www.hrodebert.com/about',
+            type: 'blog',
+            image: 'http://www.hrodebert.com/img/Me.jpg',
+            image_secure_url: 'https://www.hrodebert.com/img/Me.jpg',
+            image_width: '638',
+            image_height: '638',
+            twitter_site: '@RobertMitchel_l',
+            twitter_card: 'summary'
         });
     });
 
     // GET the article-list page with default filters.
     // Renders the article-list page with the proper meta tags for SEO.
     router.get('/article-list', function (req, res, next) {
-        const meta = JSON.parse(JSON.stringify(default_meta));
+        const meta = JSON.parse(JSON.stringify(defaultMeta));
         meta.title = 'Latest Articles | Robert Mitchell';
         meta.url = 'https://www.hrodebert.com/article-list';
         res.render(angular2Template, meta);
