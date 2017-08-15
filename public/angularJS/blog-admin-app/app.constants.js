73af5285f4df513fed8a30a3f075830dc696e6e8
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-15 11:33:39 -0500

    Made the whole of the blog-admin-app source conform to an 80 character max line length.

--- public/angularJS/blog-admin-app/app.constants.js
+++ public/angularJS/blog-admin-app/app.constants.js
@@ -1,8 +1,10 @@
 (function () {
     'use strict';
 
-    /** Defines some constants used throughout the whole app, such as the base URL of the website.
-     * ViewModes are constants to change the display according to the activity the admin is doing.
+    /** Defines some constants used throughout the whole app, such as the base
+     * URL of the website.
+     * ViewModes are constants to change the display according to the activity
+     * the admin is doing.
      *
      * TODO: Make sure each ViewMode is needed.
      */
--- public/angularJS/blog-admin-app/app.module.js
+++ public/angularJS/blog-admin-app/app.module.js
@@ -2,12 +2,13 @@
     'use strict';
 
     /** Define the 'blogApp' module.
-     * - ngRoute is needed for the controllers to determine parameters passed, such as which category of articles should
-     *         be listed, or what the ID of the article requested to be displayed is.
-     *
-     * - core contains all code which contacts the server, and any requesting of articles or categories happens through it.
-     *
-     * - articleList, articleView, and aboutView each contain their respective Views and Controllers.
+     * - ngRoute is needed for the controllers to determine parameters passed,
+     *   such as which category of articles should be listed, or what the ID of
+     *   the article requested to be displayed is.
+     * - core contains all code which contacts the server, and any requesting of
+     *   articles or categories happens through it.
+     * - articleList, articleView, and aboutView each contain their respective
+     *   Views and Controllers.
      */
     angular.module('blogAdminApp', [
         'ngRoute',
